9960066bd0cce02d0af6d3eec127a5dd146558f6
vyash5075 <vyash5075@gmail.com>
2020-12-13 15:38:04 +0530

    make home page

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const expressLayout = require("express-ejs-layouts");
 const path = require("path");
 const PORT = process.env.PORT | 4000;
 
+//Assets
+app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("home");
 });
